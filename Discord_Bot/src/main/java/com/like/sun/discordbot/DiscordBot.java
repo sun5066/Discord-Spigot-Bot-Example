dcd93f4b8e8bebd8a634563678438d5c6e26f521
sun5066 <alstjr4434@naver.com>
2020-06-23 23:21:01 +0900

    Update DiscordBot.java

--- Discord_Bot/src/main/java/com/like/sun/discordbot/DiscordBot.java
+++ Discord_Bot/src/main/java/com/like/sun/discordbot/DiscordBot.java
@@ -5,8 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DiscordBot extends JavaPlugin {
 
-    /**
-     * Author Discord : LIKE#3897
+    /** 
+     * @Author Discord : LIKE#3897
      * 제 코드를 예제로 봐주셔서 감사합니다.
      * 코딩 재밌게 하시길 바래요!
      * */
